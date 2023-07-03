548d9a2aa511c62ba9d7c1b2766bf2bb6189b3bc
Gaurav Arora <gauravarora.1100@gmail.com>
2023-07-03 11:01:14 -0500

    Adding script which can be used for removing unwanted files after running the VASP simulation.

---
+++ removing_unwanted_files/delete_unwanted_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash/
+
+find . -name "PROCAR" -type f -delete
+find . -name "*.xml" -type f -delete
+find . -name "XDATCAR" -type f -delete
+find . -name "DOSCAR" -type f -delete
+find . -name "EIGENVAL" -type f -delete
+find . -name "IBZKPT" -type f -delete
