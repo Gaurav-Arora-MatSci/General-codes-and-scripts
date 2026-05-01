2fec4e068caf8baf0269238101a783fc2df335a3
Gaurav Arora <39742952+Gaurav-Arora-MatSci@users.noreply.github.com>
2026-05-01 11:40:09 -0400

    Create submit_multiple_vasp_jobs_NERSC.sh

---
+++ submit_multiple_vasp_jobs_NERSC.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#SBATCH -N 4
+#SBATCH -C cpu
+#SBATCH -q regular
+#SBATCH -t 10:00:00
+#SBATCH --output=slurm-%A.out
+#SBATCH --job-name=1stNN
+#SBATCH --account=m4349
+##SBATCH --account=m4599
+
+# --- Write job info immediately ---
+echo "Job ID      : $SLURM_JOB_ID"   >  JOBID
+echo "Job Name    : $SLURM_JOB_NAME" >> JOBID
+echo "Submit Dir  : $SLURM_SUBMIT_DIR" >> JOBID
+echo "Start Time  : $(date)"         >> JOBID
+
+log_file="job.log_${SLURM_JOB_ID}"
+start=$(date +%s.%N)
+
+module load vasp/6.4.3-cpu
+VASP_CMD="srun -n512 -c2 --cpu_bind=cores stdbuf --output=L vasp_std"
+
+
+
+for d in calc-{0..199}; do
+    [ -d "$d" ] || continue
+    cd "$d" || continue
+
+    if [ ! -f OUTCAR ]; then
+        echo "$d - no OUTCAR, starting fresh"
+        $VASP_CMD
+
+    elif grep -q "reached required accuracy" OUTCAR; then
+        echo "$d - converged, skipping"
+        cd ..
+        continue
+
+    elif grep -q "rerun with smaller EDIFF" OUTCAR; then
+        echo "$d - restarting from CONTCAR"
+        cp CONTCAR POSCAR
+        $VASP_CMD
+
+    else
+        now=$(date +%s)
+        mtime=$(stat -c %Y OUTCAR)
+        diff=$(( now - mtime ))
+        if [ "$diff" -lt 120 ]; then
+            echo "$d - still running"
+        else
+            echo "$d - check manually"
+        fi
+	rm PROCAR *.xml DOSCAR EIGENVAL *.h5
+    fi
+
+    cd ..
+done
+
+
+#srun -n768 -c2 --cpu_bind=cores stdbuf --output=L vasp_std > "$log_file"
+
+echo "Done."
+
+duration=$(echo "$(date +%s.%N) - $start" | bc)
+execution_time=$(printf "%.2f seconds" $duration)
+echo $execution_time >> JOBID
+
