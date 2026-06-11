a0284f2db1689f06a499bd383444e7cfa983f27e
Gaurav Arora <39742952+Gaurav-Arora-MatSci@users.noreply.github.com>
2026-06-11 19:51:24 -0400

    Update submit_multiple_vasp_jobs_NERSC.sh

--- submit_multiple_vasp_jobs_NERSC.sh
+++ submit_multiple_vasp_jobs_NERSC.sh
@@ -21,49 +21,47 @@ module load vasp/6.4.3-cpu
 VASP_CMD="srun -n512 -c2 --cpu_bind=cores stdbuf --output=L vasp_gam"
 
 
-for d in calc-{0..199}; do
-    [ -d "$d" ] || continue
-    cd "$d" || continue
+module load vasp/6.4.3-cpu
+
+for d in strain*/C*/strain*; do
+(
+    [ -d "$d" ] || exit
+
+    cd "$d" || exit
 
     if [ ! -f OUTCAR ]; then
         echo "$d - no OUTCAR, starting fresh"
-        $VASP_CMD
+        srun -n512 -c2 --cpu_bind=cores stdbuf --output=L vasp_gam
 
     elif grep -q "reached required accuracy" OUTCAR; then
         echo "$d - converged, skipping"
-        cd ..
-        continue
+        exit
 
     elif grep -q "rerun with smaller EDIFF" OUTCAR; then
         echo "$d - restarting from CONTCAR"
         cp CONTCAR POSCAR
-        $VASP_CMD
+        srun -n512 -c2 --cpu_bind=cores stdbuf --output=L vasp_gam
 
     else
         now=$(date +%s)
         mtime=$(stat -c %Y OUTCAR)
-        diff=$(( now - mtime ))
+        diff=$((now - mtime))
+
         if [ "$diff" -lt 120 ]; then
             echo "$d - still running, skipping"
-            cd ..
-            continue
+            exit
         elif [ -s CONTCAR ]; then
             echo "$d - continuing from CONTCAR"
             cp CONTCAR POSCAR
-            $VASP_CMD
+            srun -n512 -c2 --cpu_bind=cores stdbuf --output=L vasp_gam
         else
             echo "$d - check manually"
         fi
     fi
 
     rm -f PROCAR *.xml DOSCAR EIGENVAL *.h5
-    cd ..
+)
 done
-
-
-#srun -n768 -c2 --cpu_bind=cores stdbuf --output=L vasp_std > "$log_file"
-
-echo "Done."
 
 duration=$(echo "$(date +%s.%N) - $start" | bc)
 execution_time=$(printf "%.2f seconds" $duration)
